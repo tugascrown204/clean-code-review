c3252fbba6e3830fab45dcf187e71b2f08714315
tugascrown204 <gajdertevet@hotmail.com>
2025-09-04 21:10:42 +0330

    Improve user input handling by validating if the provided path exists.

--- cli.js
+++ cli.js
@@ -1,11 +1,17 @@
 const { exec } = require('child_process');
 const path = require('path');
+const fs = require('fs');
 
 const args = process.argv.slice(2);
 const codePath = args[0] || '.';
 
 if (!args[0]) {
   console.error('Please provide a path to the code, using `node cli.js <path>`');
+  process.exit(1);
+}
+
+if (!fs.existsSync(codePath)) {
+  console.error('The provided path does not exist.');
   process.exit(1);
 }
 
